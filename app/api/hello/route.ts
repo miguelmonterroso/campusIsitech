2c1709850a90d6874cc39a8494f9b99918270c39
miguelmonterroso <miguelmonterroso39@gmail.com>
2024-12-26 15:56:54 -0600

    Prisma ORM added, backend init and connection with the DB

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,24 @@
+import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(){
-    return NextResponse.json({ message: 'Hello from the API'})
+export async function GET() {
+    try {
+      const users = await prisma.user.findMany();
+      return NextResponse.json(users);
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: "Error fetching users" }, { status: 500 });
+    }
+  }
+
+export async function POST() {
+    return NextResponse.json({ message: "POST request received!" });
+}
+
+export async function PUT() {
+    return NextResponse.json({ message: "PUT request received!" });
+}
+
+export async function DELETE() {
+    return NextResponse.json({ message: "DELETE request received!" });
 }
